da5daa7c4d00a0b1649022009624f14393b6af91
Nick Stebb <nickolarse@riseup.net>
2023-03-29 09:48:13 +1300

    compiles with guillems code

---
+++ dnas/bridge/zomes/coordinator/transaction_request/src/utils.rs
@@ -0,0 +1,37 @@
+use crate::holo_hash::{AgentPubKeyB64, AnyDhtHashB64};
+use bridge_integrity::Transaction;
+use hc_zome_transaction_requests_integrity::{call_transactions, TransactionRequest};
+use hdk::prelude::*;
+
+pub fn build_transaction(transaction_request_element: Element) -> ExternResult<Transaction> {
+    let transaction_request: TransactionRequest = transaction_request_element
+        .entry()
+        .to_app_option()?
+        .ok_or(WasmError::Guest(String::from(
+            "Malformed transaction_request",
+        )))?;
+
+    let spender = transaction_request.spender_pub_key.clone();
+    let recipient = transaction_request.recipient_pub_key.clone();
+
+    let spender_latest_transaction = get_latest_transaction_for_agent(spender.into())?;
+    let recipient_latest_transaction = get_latest_transaction_for_agent(recipient.into())?;
+
+    unimplemented!()
+    // let transaction = Transaction::from_previous_transactions(
+    //     transaction_request.spender_pub_key.into(),
+    //     transaction_request.recipient_pub_key.into(),
+    //     spender_latest_transaction,
+    //     recipient_latest_transaction,
+    //     transaction_request.amount,
+    //     SerializedBytes::try_from(transaction_request_element.header_address())?,
+    // )
+    // .map_err(|_| wasm_error!(WasmErrorInner::Guest("Malformed transaction".into())))?;
+    // Ok(transaction)
+}
+
+fn get_latest_transaction_for_agent(
+    agent_pub_key: AgentPubKeyB64,
+) -> ExternResult<Option<(AnyDhtHashB64, Transaction)>> {
+    call_transactions("get_latest_transaction_for_agent".into(), agent_pub_key)
+}
