c49fe27705554af5a55d60bf2211ba1842844ce0
Nick Stebb <nickolarse@riseup.net>
2023-03-31 09:36:24 +1300

    add profile store and elements

--- ui/src/holochain-app.ts
+++ ui/src/holochain-app.ts
@@ -1,5 +1,6 @@
 import { LitElement, css, html } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
+
 import {
   AppAgentWebsocket,
   ActionHash,
@@ -7,6 +8,21 @@ import {
   AgentPubKey,
 } from '@holochain/client';
 import { provide } from '@lit-labs/context';
+import { clientContext } from './contexts';
+import { Task } from '@lit-labs/task';
+import { msg } from '@lit/localize';
+
+import { AsyncStatus, StoreSubscriber } from '@holochain-open-dev/stores';
+
+import {
+  Profile,
+  ProfilesClient,
+  ProfilesStore,
+  profilesStoreContext,
+} from '@holochain-open-dev/profiles';
+import '@holochain-open-dev/profiles/elements/agent-avatar.js';
+import '@holochain-open-dev/profiles/elements/profile-prompt.js';
+import '@holochain-open-dev/profiles/elements/profile-list-item-skeleton.js';
 
 import '@shoelace-style/shoelace/dist/components/spinner/spinner.js';
 
@@ -14,8 +30,6 @@ import "./components/header-component";
 import "./components/footer-component";
 import "./components/main-component";
 
-import { Task } from '@lit-labs/task';
-import { clientContext } from './contexts';
 
 @customElement('holochain-app')
 export class HolochainApp extends LitElement {
@@ -23,9 +37,14 @@ export class HolochainApp extends LitElement {
 
   @provide({ context: clientContext })
   @property({ type: Object })
-  client!: AppAgentClient;
+  _client!: AppAgentClient;
 
-  _init = new Task(this, () => this.client.callZome({
+  @provide({ context: profilesStoreContext })
+  @property()
+  _profilesStore!: ProfilesStore;
+
+  _myProfile!: StoreSubscriber<AsyncStatus<Profile | undefined>>;
+  _init = new Task(this, () => this._client.callZome({
     cap_secret: null,
     role_name: 'bridge',
     zome_name: 'bridge',
@@ -35,9 +54,46 @@ export class HolochainApp extends LitElement {
 
   async firstUpdated() {
     // We pass '' as url because it will dynamically be replaced in launcher environments
-    this.client = await AppAgentWebsocket.connect('', 'bridge.hc');
 
+    this._client = await AppAgentWebsocket.connect('', 'bridge.hc');
+    await this.initStores(this._client);
+    
     this.loading = false;
+  }
+
+  async initStores(appAgentClient: AppAgentClient) {
+    this._profilesStore = new ProfilesStore(
+      new ProfilesClient(appAgentClient, 'bridge')
+    );
+    this._myProfile = new StoreSubscriber(
+      this,
+      () => this._profilesStore.myProfile
+    );
+  }
+
+  renderMyProfile() {
+    switch (this._myProfile.value.status) {
+      case 'pending':
+        return html`<profile-list-item-skeleton></profile-list-item-skeleton>`;
+      case 'complete':
+        const profile = this._myProfile.value.value;
+        if (!profile) return html``;
+
+        return html`<div
+          class="row"
+          style="align-items: center;"
+          slot="actionItems"
+        >
+          <agent-avatar .agentPubKey=${this._client.myPubKey}></agent-avatar>
+          <span style="margin: 0 16px;">${profile?.nickname}</span>
+        </div>`;
+      case 'error':
+        return html`<display-error
+          .headline=${msg('Error fetching your profile')}
+          .error=${this._myProfile.value.error.data.data}
+          tooltip
+        ></display-error>`;
+    }
   }
 
   render() {
